1c72b9476078306913f5efa5d57fa5fe5fce98af
ContrerasGael <132929166+ContrerasGael@users.noreply.github.com>
2023-05-29 08:04:19 -0600

    variables globales

---
+++ variables globales.cpp
@@ -0,0 +1,28 @@
+/*Programa numero
+      titulo:Vriables goblales
+      fecha:29 05 23
+      nombre: Gael Contreras Gonzalez */
+	  
+#include <stdio.h>
+int a, b, c;
+int main ()
+{
+	int valor;
+	printf("Tres valores"):
+	scanf ("%d %d %d/a", &a,&b,&c);
+	valor a+b+c;
+	printf("valor de a: %d/n", a);
+	printf("valor de b: %d/n", b);
+	printf("valor de c: %d/n", c);
+	printf("suma de las variables: %d", valor):
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+}	  
+	       
