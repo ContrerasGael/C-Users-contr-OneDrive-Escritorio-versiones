8280411cb63fd64c1f3846f4a209cab8ad5d9350
ContrerasGael <132929166+ContrerasGael@users.noreply.github.com>
2023-05-29 07:53:31 -0600

    luna_kilo

---
+++ luna_kilo.cpp
@@ -0,0 +1,20 @@
+/*programa numero 
+ 	 titulo:numero de interacciones
+ 	 fecha:29 05 23 
+ 	 nombre:Gael Contreras Gonzalez */
+ 
+ #include <Stdio.h>
+ 
+ int main()
+ {
+ 	const int luna= 238857;
+ 	float luna_kilo; //distancia en millas
+ 	pritf ("Distancia a la luna %d millas/n", luna);
+ 	luna_kilo=luna*1.609; /*una milla es igual a 1.609 kilometro*/
+ 	printf("en kilometros es %.2fkm. /n", luna_kilo);
+ 	
+ 	
+ 	
+ 	return 0:
+ 	
+ }	 
