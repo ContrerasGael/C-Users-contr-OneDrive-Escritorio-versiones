8399d93efcd2bac983980e2bb395bcfe422d856c
ContrerasGael <132929166+ContrerasGael@users.noreply.github.com>
2023-05-30 10:07:43 -0600

    Ventas 30/05/2023

---
+++ ventas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+
+	int ventas;
+	int anios;
+	int sal_min;
+	int IVA;
+	ventas=5000;
+	anios=8;
+	sal_min=1000;
+	IVA=15;
+	if (ventas<sal_min*3&&anios>>10*IVA);
+	
+	printf("resultado=%d");
+	return 0;
+}
