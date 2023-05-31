52aced4299a0278e9f35186b265d2866a320bc29
ContrerasGael <132929166+ContrerasGael@users.noreply.github.com>
2023-05-31 11:55:10 -0600

    azaer v2 31/05/2023

---
+++ azr v2.c
@@ -0,0 +1,14 @@
+/* programa numero 7
+   titulo: fUNCION IF (prueba logica)
+   fecha: 31/05/23
+   nombre: Oscar Zempoaltecatl Santillan*/
+	#include <stdio.h>
+int numsecreto = 7;
+int main ()
+
+{
+printf ("ingresa un numero al azar:");	
+scanf ("%f, &numsecreto");
+if ((numsecreto=1)? (printf ("adivinaste el numero")):(printf ("incorrecto")));
+return 0;
+}
