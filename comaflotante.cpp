8c2bee0a184b89124b7a83238087c5773b5c6c82
ContrerasGael <132929166+ContrerasGael@users.noreply.github.com>
2023-05-31 07:44:15 -0600

    ejercicio1 31/05/2023

---
+++ comaflotante.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main
+{
+	printf("El tamaño de la variables de coma flotante es %d", size);
+	printf(" bytes\n ");
+    printf("El tamaño de la variables de doble precision  es %d", size);
+    printf(" bytes\n ");
+    return 0;
+}
